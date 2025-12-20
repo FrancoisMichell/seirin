4cf1fb5ba77c6a46e289b3a5fe5859904f42c6a5
Francois <francois_michell95@hotmail.com>
2025-12-20 14:48:27 -0300

    feat: adequate update fields and adding tests

---
+++ db/migrations/1734537600000-UpdateUserNullableFields.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UpdateUserNullableFields1734537600000 implements MigrationInterface {
+  name = 'UpdateUserNullableFields1734537600000';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "registry" DROP NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "password" DROP NOT NULL`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "password" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "registry" SET NOT NULL`,
+    );
+  }
+}
